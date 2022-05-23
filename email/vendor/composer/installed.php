6d2d124ac3c3789485bdd8f7fd5d4e14e966e012
Kedyson81 <kedysonf@gmail.com>
2022-05-23 00:33:31 -0300

    recuperar senha

---
+++ email/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a8c0f8ef6d4941847947aa0a4732369707603712',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a8c0f8ef6d4941847947aa0a4732369707603712',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v5.2.28',
+            'version' => '5.2.28.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'acba50393dd03da69a50226c139722af8b153b11',
+            'dev_requirement' => false,
+        ),
+    ),
+);
